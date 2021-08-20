d356bf43c2a7c3c30a867f1b6f52331be5999774
Sharma Ji <devvrat.sharm_cs19@gla.ac.in>
2021-08-20 10:51:17 +0530

    added all pages template

--- src/main/java/com/brahmastra/samanlelo/controller/HomeController.java
+++ src/main/java/com/brahmastra/samanlelo/controller/HomeController.java
@@ -23,9 +23,69 @@ public class HomeController {
         return "index";
     }
 
+    @RequestMapping("/grocery")
+    public String grocery(Model model){
+        model.addAttribute("title","Grocery");
+        return "grocery";
+    }
+
+    @RequestMapping("/pantry")
+    public String pantry(Model model){
+        model.addAttribute("title","Pantry");
+        return "pantry";
+    }
+
+    @RequestMapping("/fruits")
+    public String fruits(Model model){
+        model.addAttribute("title","Fruits");
+        return "fruits";
+    }
+
+    @RequestMapping("/vegetables")
+    public String vegetables(Model model){
+        model.addAttribute("title","Vegetables");
+        return "vegetable";
+    }
+
+    @RequestMapping("/dairy")
+    public String dairy(Model model){
+        model.addAttribute("title","Dairy");
+        return "dairy";
+    }
+
+    @RequestMapping("/frozenFood")
+    public String frozenFood(Model model){
+        model.addAttribute("title","Frozen Food");
+        return "frozenFood";
+    }
+
+    @RequestMapping("/login")
+    public String login(Model model){
+        model.addAttribute("title","Login");
+        return "login";
+    }
+
+    @RequestMapping("/user/profile")
+    public String profile(Model model){
+        model.addAttribute("title","Profile");
+        return "profile";
+    }
+
     @RequestMapping("/user/cart")
     public String checkout(Model model){
         model.addAttribute("title","Cart");
         return "checkout";
+    }
+
+    @RequestMapping("/user/currentOrder")
+    public String currentOrder(Model model){
+        model.addAttribute("title","Current Order");
+        return "currentOrder";
+    }
+
+    @RequestMapping("/user/previousOrder")
+    public String previousOrder(Model model){
+        model.addAttribute("title","Previous Order");
+        return "previousOrder";
     }
 }
